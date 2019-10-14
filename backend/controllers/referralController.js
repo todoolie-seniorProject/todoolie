b283836c9292c8b8baaa37d513cdea9a1d258e11
fariha <fp5975@wayne.edu>
2019-10-14 13:46:18 -0400

    added refferal adding API

--- backend/controllers/referralController.js
+++ backend/controllers/referralController.js
@@ -9,3 +9,21 @@ exports.list_all_referrals = function(req, res) { // controller function which w
       res.send(task); //simply send all the referrals on user's screen
     });
 };
+
+exports.create_new_referral = function(req, res) { // controller function which will call model function to create new referral
+
+    if(!req.body.username || !req.body.rollnum){ // check if username and rollnum are provided, otherwise give error
+ 
+             res.status(400).send({ error:true, message: "enter username and rollnum" }); //err msg in case username/rollnum missing
+ 
+         }
+ else{
+   
+   Task.createReferral(req.body, function(err, task) { //this is calling function createRefferal in model file
+     if (err)
+       res.send(err); // if error occurs, send error
+     else
+        res.send(task); // if successful, send response to user
+   });
+}
+};
--- backend/models/referralsModel.js
+++ backend/models/referralsModel.js
@@ -23,4 +23,20 @@ Task.getAllReferrals = function (result) {
         });   
 };
 
+Task.createReferral = function (body, result) {    // model function which will actually insert ddata in referral table
+    //the below is the query to insert data, it takes from user the username and rollnum and set status 0 as in start
+    //the status of the referral will be 0, it will later be changed on verification
+    sql.query("INSERT INTO Referrals (Username, Rollnum, Status) values (?,?,0)", [body.username, body.rollnum], function (err, res) {
+            
+            if(err) {
+                console.log("error: ", err); // iff error occurs, show
+                result(err, null);
+            }
+            else{
+                console.log(res);
+                result(null, res); //if no error occurs, show res which means response
+            }
+        });           
+};
+
 module.exports= Task;
