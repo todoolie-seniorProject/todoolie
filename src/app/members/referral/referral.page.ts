c27702e7def76fb6996df8182a3976fa1e022848
Unknown <em8878@wayne.edu>
2019-11-17 12:00:30 -0500

    added missing stripeContller file and small tweaks to the alerts for referral

--- src/app/members/referral/referral.page.ts
+++ src/app/members/referral/referral.page.ts
@@ -80,7 +80,7 @@ public school: string;
 
   async showAlert(msg) {
     const alert = await this.alertCtrl.create({
-      header: 'Server Message',
+      header: 'Error!',   // show error alert instead of server message
       message: msg,
       buttons: ['OK']
     });
@@ -107,27 +107,7 @@ async sendMail() {
 
 
 
-
-
-// sendMail function
-// sendMail( name: string, email: string, age: number, school: string): any {
-//   const user = { "name" : name, "age": age, "email": email, "school": school}
-//   this.http.post('http://localhost:3000/sendmail', user).subscribe(
-//     data => {
-//       const res: any = data;
-//       console.log(
-//         `👏 > 👏 > 👏 > 👏 ${user.name} is successfully register and mail has been sent and the message id is ${res.messageId}`
-//       );
-//     },
-//     err => {
-//       console.log(err);
-//       console.log("failed to send email to student referral")
-//     }
-//   );
-// }
-
-
-clear(){
+clear() {
   this.name = '';
   this.email = '';
   this.school = '';
